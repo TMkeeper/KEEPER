f03bc34e20c5e24c6a6fece0e218adf5c81ca724
karraradnan <33726547+karraradnan@users.noreply.github.com>
2017-11-17 00:23:13 +0300

    Update run.lua

--- run.lua
+++ run.lua
@@ -1,10 +1,61 @@
-#!/bin/bash 
- COUNTER=0
-while [  $COUNTER -lt 5 ]; do
-kill $(pgrep telegram-cli)
-echo -e "\e[38;5;77m"   
-echo -e "        \e[38;5;40m"
-sleep 2
-   ./tg -s ./bot.lua
-sleep 3
-done
+#!/usr/bin/env bash
+token="399096444:AAHav14judfKas9nU1lKGMa9pOxvRS84BrY"
+function print_logo() {
+	echo -e "\e[38;5;77m"   
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+}
+
+if [ ! -f ./tg ]; then
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+    echo "\e[31;1mtg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+if [ ! $token ]; then
+echo -e ""
+echo -e 
+echo -e redis-
+echo -e 
+echo -e 
+echo -e 
+echo -e
+  echo -e "\e[31;1mToken Not found\e"
+ exit 1
+ fi
+
+
+  print_logo
+   echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e
+echo -e
+echo -e 
+echo -e 
+echo -e
+echo -e 
+echo -e    
+
+curl "https://api.telegram.org/bot"$token"/sendmessage" -F
+./tg -s ./bot.lua $@ --bot=$token
