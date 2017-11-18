7ef7761fa45cde5770153a2adb0233dc17c3ba8c
karraradnan <33726547+karraradnan@users.noreply.github.com>
2017-11-18 13:42:37 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,53 +1,82 @@
+#!/usr/bin/env bash
+if [ ! -f ./td ]; then
+  wget "https://valtman.name/files/telegram-cli-1222"
+mv telegram-cli-1222 td
+sudo apt-get update 
+sudo apt-get upgrade
+sudo apt-get install libreadline-dev libssl-dev lua5.2 liblua5.2-dev git make unzip redis-server curl libcurl4-gnutls-dev
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev make autoconf unzip git redis-server g++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt-get install libreadline-dev
+sudo apt install libreadline-dev 
+sudo apt-get install libreadline6 
+sudo apt-get install libreadline6-dev
+sudo apt-get install libconfig-dev 
+sudo apt-get install libssl-dev 
+sudo apt-get install lua5.2 
+sudo apt-get install liblua5.2-dev 
+sudo apt-get install lua-socket 
+sudo apt-get install lua-sec 
+sudo apt-get install lua-expat
+sudo luarocks install luautf8
+sudo apt-get install libevent-dev 
+sudo apt-get install redis-server 
+sudo apt-get install libjansson-dev 
+sudo apt-get install libpython-dev 
+sudo apt-get install expat libexpat1-dev 
+sudo apt-get install psmisc sysvinit-utils
 sudo apt-get update
-
-sudo apt-get upgrade -y
-
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev  -y
-
-sudo apt-get install lua-lgi -y
-
-sudo apt-get install software-properties-common -y
-
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-
-sudo apt-get install libstdc++6 -y
-
-sudo apt-get update -y
-
-sudo apt-get upgrade -y
-
-sudo apt-get dist-upgrade -y
-
-wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
- tar zxpf luarocks-2.2.2.tar.gz
- rm luarocks-2.2.2.tar.gz
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get install libreadline-dev
+sudo apt install libreadline-dev 
+sudo apt-get install libreadline6 
+sudo apt-get install libreadline6-dev
+sudo apt-get install libconfig-dev 
+sudo apt-get install libssl-dev 
+sudo apt-get install lua5.2 
+sudo apt-get install liblua5.2-dev 
+sudo apt-get install lua-socket 
+sudo apt-get install lua-sec 
+sudo apt-get install lua-expat 
+sudo apt-get install libevent-dev 
+sudo apt-get install make 
+sudo apt-get install unzip 
+sudo apt-get install git 
+sudo apt-get install redis-server 
+sudo apt-get install autoconf 
+sudo apt-get install g++ 
+sudo apt-get install libjansson-dev 
+sudo apt-get install libpython-dev 
+sudo apt-get install expat libexpat1-dev 
+sudo apt-get install ppa-purge 
+sudo apt-get install python3-pip 
+sudo apt-get install python3-dev
+sudo apt-get install software-properties-common
+sudo apt-get install python-software-properties
+sudo apt-get install gcc-6
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo ppa-purge
+sudo service redis-server restart
+sudo apt-get update 
+sudo apt-get upgrade
+ sudo apt-get install libreadline-dev libssl-dev lua5.2 liblua5.2-dev git make unzip redis-server curl libcurl4-gnutls-dev
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz 
+ tar zxpf luarocks-2.2.2.tar.gz 
  cd luarocks-2.2.2
- ./configure; sudo make bootstrap
- sudo luarocks install luasocket
- sudo luarocks install luasec
- sudo luarocks install redis-lua
- sudo luarocks install lua-term
- sudo luarocks install serpent
- sudo luarocks install dkjson
- sudo luarocks install lanes
- sudo luarocks install Lua-cURL
- sudo service redis-server start
- sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
- sudo apt-get install g++-4.7 -y c++-4.7
- sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
- sudo apt-get install screen -y
- sudo apt-get install tmux -y
- sudo apt-get install libstdc++6 -y
- sudo apt-get install lua-lgi -y
- sudo apt-get install libnotify-dev -y
- sudo apt-get install python-setuptools python-dev build-essential -y
- sudo easy_install pip
- sudo pip install redis
- cd ..
- cd KEEPER
- rm -rf "luarocks-2.2.2"
- wget "https://valtman.name/files/telegram-cli-1222"
- mv telegram-cli-1222 tg
- chmod +x tg
- chmod +x run.sh
-./run.sh
+ ./configure; sudo make bootstrap 
+sudo luarocks install luasec 
+sudo luarocks install luasocket 
+sudo luarocks install redis-lua 
+sudo luarocks install lua-term 
+sudo luarocks install serpent
+sudo luarocks install dkjson 
+sudo luarocks install Lua-cURL
+sudo luarocks install luautf8
+  chmod +x td
+  ./td -s KEEPER.lua
+fi
+./td -s KEEPER.lua
